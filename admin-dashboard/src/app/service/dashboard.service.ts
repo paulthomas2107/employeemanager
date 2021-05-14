22a1b3abeca8a89e6ef9c9b9874168b5dcdbd01c
Paul Thomas <paulthomas2107@gmail.com>
2021-05-14 16:04:09 +0100

    More

---
+++ admin-dashboard/src/app/service/dashboard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  private String SERVER_URL = environment.serverUrl;
+
+
+  constructor(private http: HttpClient) {
+
+   }
+
+   public getHTTPTraces(): Observable<any> {
+          return this.http.get(`${this.SERVER_URL}/httptrace`);
+   }
+}
---
+++ src/main/java/tech/getarrays/employeemanager/monitoring/Main.java
@@ -0,0 +1,9 @@
+package tech.getarrays.employeemanager.monitoring;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("java.version"));
+    }
+}
